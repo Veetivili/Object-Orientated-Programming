1bd75444e80b9658d7fce97f7d67b6418ccf8451
Veeti Hakala <AB0197@Labranet.jamk.fi>
2022-09-15 10:18:56 +0300

    Started T9

---
+++ T9 Vehicle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace T9_Vehicle
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public int Tires { get; set; }
+
+        public Vehicle (string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+        public Vehicle (string brand, string model, int speed, int tires): this (brand, model)
+        {
+            Speed = speed;
+            Tires = tires;
+        }
+        public string ShowInfo()
+        {
+            return $"Brand: {Brand}, Model: {Model}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
