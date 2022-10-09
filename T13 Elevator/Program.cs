2eb3b9163721acff79c8e1776a627f8cb7bc1564
Veeti Hakala <AB0197@student.jamk.fi>
2022-10-09 13:02:49 +0300

    Finished exercise T13 and T14

---
+++ T13 Elevator/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace T13_Elevator
+{
+    internal class Program
+    {
+        public class Elevator 
+        {
+            public int CurrentFloor { get; private set; } = 1;
+            public void ChangeFloor(int floor, out string message)
+            {
+                Console.WriteLine("Give a new floor number (1-5)");
+                if (floor < 0)
+                {
+                    message = $"{floor}\nFloor is too Small";
+                }
+                else if (floor > 5) 
+                {
+                    message = $"{floor}\nFloor is too Big!";
+                }
+                else
+                {
+                    CurrentFloor = floor;
+                    message = $"{floor}\nElevator is now in floor: {CurrentFloor}";
+                }
+                
+                //message = "Give a new floor";
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator();
+            string message = "";
+            Console.WriteLine($"Elevator is now in: {elevator.CurrentFloor}");
+            int floor = 2;
+            elevator.ChangeFloor(floor, out message);
+            Console.WriteLine(message);
+            floor = -1;
+            elevator.ChangeFloor(floor, out message);
+            Console.WriteLine(message);
+            floor = 6;
+            elevator.ChangeFloor(floor, out message);
+            Console.WriteLine(message);
+            floor = 5;
+            elevator.ChangeFloor(floor, out message);
+            Console.WriteLine(message);
+
+        }
+    }
+}
---
+++ T14 Amplifier/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace T14_Amplifier
+{
+   
+    internal class Program
+    {
+        public class Amplifier
+        {
+            public int CurrentVolume { get; private set; }
+            public void ChangeVolume(int volume, out string message)
+            {
+                Console.WriteLine("Give a new volume value (0-100)");
+                if (volume < 0)
+                {
+                    CurrentVolume = 0;
+                    message = $"{volume}\nToo low volume - Amplifier volume is set to minimum: {CurrentVolume}";
+                }
+                else if (volume > 100)
+                {
+                    CurrentVolume = 100;
+                    message = $"{volume}\nToo big Volume - Amplifier volume is set to maximum: {CurrentVolume}";
+                }
+                else
+                {
+                    CurrentVolume = volume;
+                    message = $"{volume}\nAmplifier Volume is set to: {CurrentVolume}";
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Amplifier amplifier = new Amplifier();
+            string message = "";
+            Console.WriteLine($"Volume is now: {amplifier.CurrentVolume}");
+            int volume = 100;
+            amplifier.ChangeVolume(volume, out message);
+            Console.WriteLine(message);
+            volume = 40;
+            amplifier.ChangeVolume(volume, out message);
+            Console.WriteLine(message);
+            volume = 0;
+            amplifier.ChangeVolume(volume, out message);
+            Console.WriteLine(message);
+            volume = -10;
+            amplifier.ChangeVolume(volume, out message);
+            Console.WriteLine(message);
+            volume = 200;
+            amplifier.ChangeVolume(volume, out message);
+            Console.WriteLine(message);
+            volume = 35;
+            amplifier.ChangeVolume(volume, out message);
+            Console.WriteLine(message);
+
+
+        }
+    }
+}
